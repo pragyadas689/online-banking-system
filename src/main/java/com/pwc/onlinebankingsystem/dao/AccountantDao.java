19ac1f8616837256799bbae823e3921f65fb8ad5
Pragya Das <pragyadas689@gmail.com>
2023-10-13 08:17:07 +0530

    Update AccountantDao.java

--- src/main/java/com/pwc/onlinebankingsystem/dao/AccountantDao.java
+++ src/main/java/com/pwc/onlinebankingsystem/dao/AccountantDao.java
@@ -3,8 +3,18 @@ package com.pwc.onlinebankingsystem.dao;
 import com.pwc.onlinebankingsystem.entity.Accountant;
 import com.pwc.onlinebankingsystem.exception.AccountantException;
 import com.pwc.onlinebankingsystem.exception.CustomerException;
+import com.pwc.onlinebankingsystem.entity.Customer;
 
 public interface AccountantDao {
        public Accountant LoginAccountant(String accountantUsername,String accountantPassword)  throws AccountantException ;
        public int addCustomer(String customerName,String customerMail,String customerPassword,String customerMobile,String customerAddress) throws CustomerException;
+       public String addAccount(int customerBalance,int cid) throws CustomerException;
+       public String updateCustomer(int customerAccountNumber, String customerAddress) throws CustomerException;
+       public String deleteAccount (int customerAccountNumber) throws CustomerException;
+       public Customer viewCustomer (int customerAccountNumber) throws CustomerException;
+
+       public Customer viewAllCustomer() throws CustomerException;
+
+
+
 }
