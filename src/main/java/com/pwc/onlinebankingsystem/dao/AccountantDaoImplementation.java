54d24aa8b939d8a795aa333a8b416493fcf2c12d
Pragya Das <pragyadas689@gmail.com>
2023-10-13 08:19:32 +0530

    Update AccountantDaoImplementation.java

--- src/main/java/com/pwc/onlinebankingsystem/dao/AccountantDaoImplementation.java
+++ src/main/java/com/pwc/onlinebankingsystem/dao/AccountantDaoImplementation.java
@@ -2,6 +2,7 @@ package com.pwc.onlinebankingsystem.dao;
 
 import com.pwc.onlinebankingsystem.databaseconnection.DatabaseConnection;
 import com.pwc.onlinebankingsystem.entity.Accountant;
+import com.pwc.onlinebankingsystem.entity.Customer;
 import com.pwc.onlinebankingsystem.exception.AccountantException;
 import com.pwc.onlinebankingsystem.exception.CustomerException;
 
@@ -50,16 +51,172 @@ public class AccountantDaoImplementation implements AccountantDao{
             ps.setString(5,customerAddress);
 
             int x = ps.executeUpdate();
+
             if(x>0){
-                System.out.println("customer successfully added");
+                PreparedStatement ps2 = conn.prepareStatement("select cid from customerinformation where customerMail = ? and customerMobile = ? ");
+                ps2.setString(1,customerMail);
+                ps2.setString(2,customerMobile);
+
+                ResultSet rs = ps2.executeQuery();
+
+                if(rs.next()) {
+                    cid = rs.getInt("cid");
+                }
+                else{
+                    System.out.println("Inserted Data is incorrect please try again");
+                }
             }
-            else{
-                System.out.println("Customer not added successfully");
-            }
+
         }
         catch (SQLException e){
             System.out.println("SQL Query Related Problem");
         }
+
+
         return cid;
     }
+
+    @Override
+    public String addAccount(int customerBalance, int cid) throws CustomerException {
+        String message = null;
+        try(Connection conn = DatabaseConnection.provideConnection()) {
+            PreparedStatement ps = conn.prepareStatement("insert into account (customerBalance,cid) values(?,?) ");
+
+            ps.setInt(1,customerBalance);
+            ps.setInt(2,cid);
+
+            int x = ps.executeUpdate();
+            if(x>0){
+                System.out.println("Account added successfully!!");
+            }
+            else {
+                System.out.println("Failure not added!!!");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("SQL Related Exception");
+        }
+        return message;
+    }
+
+    @Override
+    public String updateCustomer(int customerAccountNumber, String customerAddress) throws CustomerException {
+        String message = null;
+        try(Connection conn = DatabaseConnection.provideConnection()){
+            PreparedStatement ps = conn.prepareStatement("update customerinformation i inner join account a on i.cid = a.cid and a.customerAccountNumber = ? set i.customerAddress = ? ");
+
+            ps.setInt(1,customerAccountNumber);
+            ps.setString(2,customerAddress);
+
+            int x = ps.executeUpdate();
+            if(x>0){
+                System.out.println("Address updated successfully.....");
+            }
+            else{
+                System.out.println("Customer updation is not successfull.....");
+                System.out.println("----------------------------------------");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            message = e.getMessage();
+        }
+        return message;
+    }
+
+    @Override
+    public String deleteAccount(int customerAccountNumber) throws CustomerException {
+       String message = null;
+       try(Connection conn = DatabaseConnection.provideConnection()){
+           PreparedStatement ps = conn.prepareStatement("delete i from customerinformation i inner join account a on i.cid = a.cid where a.customerAccountNumber = ? ");
+           ps.setInt(1,customerAccountNumber);
+           int x = ps.executeUpdate();
+           if(x>0){
+               System.out.println("Customer Account deleted successfully....");
+           }
+           else{
+               System.out.println("Deletion failed..........Account Not Found");
+               System.out.println("-------------------------------------------");
+           }
+       }
+       catch (SQLException e){
+           e.printStackTrace();
+           message = e.getMessage();
+       }
+       return message;
+    }
+
+    @Override
+    public Customer viewCustomer(int customerAccountNumber) throws CustomerException {
+        Customer cu = null;
+        try(Connection conn = DatabaseConnection.provideConnection()){
+            PreparedStatement ps = conn.prepareStatement("select * from customerinformation i inner join account a on a.cid = i.cid where customerAccountNumber = ? ");
+
+            ps.setInt(1,customerAccountNumber);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){
+                int a = rs.getInt("customerAccountNumber");
+                String n = rs.getString("customerName");
+                int b = rs.getInt("customerBalance");
+                String e = rs.getString("customerPassword");
+                String p = rs.getString("customerMail");
+                String  m = rs.getString("customerMobile");
+                String ad = rs.getString("customerAddress");
+
+                cu = new Customer(a,n,b,e,p,m,ad);
+            }
+            else{
+                throw new CustomerException("Invalid Account Number......");
+
+            }
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return cu;
+
+    }
+
+    @Override
+    public Customer viewAllCustomer() throws CustomerException {
+        Customer cu = null;
+        try(Connection conn = DatabaseConnection.provideConnection()){
+            PreparedStatement ps = conn.prepareStatement("select * from customerinformation i inner join account a on a.cid = i.cid  ");
+
+
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                int a = rs.getInt("customerAccountNumber");
+                String n = rs.getString("customerName");
+                int b = rs.getInt("customerBalance");
+                String e = rs.getString("customerPassword");
+                String p = rs.getString("customerMail");
+                String  m = rs.getString("customerMobile");
+                String ad = rs.getString("customerAddress");
+
+
+                System.out.println("************************************");
+                System.out.println("Account Number : "+a);
+                System.out.println("Account Balance : "+b);
+                System.out.println("Customer Name : "+n);
+                System.out.println("Customer Mail : "+p);
+                System.out.println("Customer Mobile : "+m);
+                System.out.println("Customer Password :"+e);
+                System.out.println("Customer Address : "+ad);
+                System.out.println("**************************************");
+
+                cu = new Customer(a,n,b,e,p,m,ad);
+            }
+
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return cu;
+    }
+
 }
